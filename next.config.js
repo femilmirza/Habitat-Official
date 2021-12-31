1c52b5e23f850c0a66dee13e330bfed0f2869421
LucidMach <nukalasuraj7@gmail.com>
2021-12-31 21:58:13 +0530

    PWA

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+module.exports = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
   reactStrictMode: true,
-});
+};
